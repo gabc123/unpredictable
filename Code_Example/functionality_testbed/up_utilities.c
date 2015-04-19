b94faa573cf723070d07029fd196e0f1edbd2c2c
o_0 <somtingsomting@gmail.com>
2015-04-19 17:48:02 +0200

    Added utilities c and h files, to provide a place to have all general function that not directly game related.

---
+++ Code_Example/functionality_testbed/up_utilities.c
@@ -0,0 +1,8 @@
+//
+//  util.c
+//
+//  Created by o_0 on 2015-04-19.
+//  Copyright (c) 2015 o_0. All rights reserved.
+//
+
+#include "util.h"
---
+++ Code_Example/functionality_testbed/up_utilities.h
@@ -0,0 +1,14 @@
+//
+//  util.h
+//
+//  Created by o_0 on 2015-04-19.
+//  Copyright (c) 2015 o_0. All rights reserved.
+//
+
+#ifndef UP_UTILITIES_H
+#define UP_UTILITIES_H
+
+#include "up_vertex.h"
+
+
+#endif /* UP_UTILITIES_H */
