ff1a5e48e3ded63a0c36d3713b10d706f9b3fbee
tobde <tobde@users.noreply.github.com>
2015-04-21 13:57:30 +0200

    Vertex change to bugtest linux functionallity

--- Code_Example/functionality_testbed/up_vertex.c
+++ Code_Example/functionality_testbed/up_vertex.c
@@ -48,12 +48,12 @@ struct up_mesh *UP_mesh_new(struct up_vertex *vertex, int vertex_count,unsigned 
 	{
 		UP_ERROR_MSG("Full mesh \n");
 	}
-	struct up_mesh *mesh = &(internal_state.mesh[internal_state.mesh_count]);
+	struct up_mesh *mesh = malloc(sizeof(struct up_mesh));//&(internal_state.mesh[internal_state.mesh_count]);
 	mesh->vertex_count = vertex_count;
     mesh->index_count = index_count;
     printf("mesh->vertex_count:%d \n",mesh->vertex_count);
 
-	
+
 
     struct up_vec3 *positions = malloc(sizeof(struct up_vec3) * mesh->vertex_count);
     if (positions == NULL) {
@@ -61,14 +61,14 @@ struct up_mesh *UP_mesh_new(struct up_vertex *vertex, int vertex_count,unsigned 
         return NULL;
     }
     printf("after pos coord malloc\n");
-    
+
     struct up_vec2 *textureCoord = malloc(sizeof(struct up_vec2) * mesh->vertex_count);
     if (textureCoord == NULL) {
         UP_ERROR_MSG("malloc failed, textureCoord");
         return NULL;
     }
     printf("after text coord malloc\n");
-    
+
     int i = 0;
     for (i = 0; i< mesh->vertex_count; i++) {
         positions[i] = vertex[i].pos;
@@ -81,35 +81,35 @@ struct up_mesh *UP_mesh_new(struct up_vertex *vertex, int vertex_count,unsigned 
     glGenVertexArrays(1, &(mesh->vertexArrayObj));
     printf("after genvertex \n");
     glBindVertexArray(mesh->vertexArrayObj);
-    
+
 	glGenBuffers(MESH_BUFFER_COUNT, mesh->vertexArrayBuffer);
-    
+
     // binding the position
 	glBindBuffer(GL_ARRAY_BUFFER, mesh->vertexArrayBuffer[MESH_BUFFER_VB]);
 	glBufferData(GL_ARRAY_BUFFER, mesh->vertex_count * sizeof(positions[0]), &positions[0],GL_STATIC_DRAW);
-    
+
     //vilken attribuit array, hurmånga element, vad för typ, hur mycket som ska skippas mellan varje
     // och vilken offset från starten
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
-    
+
     // binding the texture coordenates
     glBindBuffer(GL_ARRAY_BUFFER, mesh->vertexArrayBuffer[MESH_BUFFER_TXB]);
     glBufferData(GL_ARRAY_BUFFER, mesh->vertex_count * sizeof(textureCoord[0]), &textureCoord[0],GL_STATIC_DRAW);
-    
+
     //vilken attribuit array, hurmånga element, vad för typ, hur mycket som ska skippas mellan varje
     // och vilken offset från starten
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0);
-    
+
     // binding the indecies
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mesh->vertexArrayBuffer[MESH_BUFFER_INDEX]);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, mesh->index_count * sizeof(indexArray[0]), &indexArray[0],GL_STATIC_DRAW);
-    
+
     glBindVertexArray(0);
-    
-    
+
+
 	internal_state.mesh_count++;
     free(positions);
     free(textureCoord);
@@ -124,7 +124,7 @@ void up_draw_mesh(struct up_mesh *mesh)
 
 	//glDrawArrays(GL_TRIANGLES,0,mesh->vertex_count);
     glDrawElementsBaseVertex(GL_TRIANGLES, mesh->index_count, GL_UNSIGNED_INT, 0, 0);
-    
+
 	glBindVertexArray(0);	//fri gör
 }
 
