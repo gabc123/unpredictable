b53721105e3d01c9a0e4a141dedf527fa59010f7
tobde <tobde@users.noreply.github.com>
2015-04-22 14:30:13 +0200

    partial loading of .obj file format data.

--- Code_Example/functionality_testbed/up_objectReader.c
+++ Code_Example/functionality_testbed/up_objectReader.c
@@ -8,6 +8,8 @@
 #include "filereader/up_filereader.h"
 
 void vertexPositionsData(char *rad, struct up_generic_list *vertexPosList);
+void texturePositionsData(char *rad, struct up_generic_list *texturePosList);
+void normalPositionsData(char *rad, struct up_generic_list *normalPosList);
 
 struct up_objModel *up_loadObjModel(const char *filename)
 {
@@ -16,6 +18,7 @@ struct up_objModel *up_loadObjModel(const char *filename)
     char *rad;
     struct up_generic_list *vertexPosList = up_vec3_list_new(20);
     struct up_generic_list *texturePosList = up_vec2_list_new(20);
+    struct up_generic_list *normalPosList = up_vec3_list_new(20);
 
     struct UP_textHandler objFile = up_loadObjFile(filename);
     text=objFile.text;
@@ -29,7 +32,7 @@ struct up_objModel *up_loadObjModel(const char *filename)
             UP_ERROR_MSG("ERROR, obj String could not be found.");
             break;
         }
-        if(rad[0]=='v' && rad[1] == ' ')
+        if(rad[0]=='v')
         {
             switch (rad[1])
             {
@@ -39,7 +42,11 @@ struct up_objModel *up_loadObjModel(const char *filename)
                     break;
                 case 't':
                     rad+=3;
-                    texturePositionsData(rad, vertexPosList);
+                    texturePositionsData(rad, texturePosList);
+                    break;
+                case 'n':
+                    rad+=3;
+                    normalPositionsData(rad, normalPosList);
                     break;
                 default:
                     UP_ERROR_MSG_STR("Unknown identifier, look in obj file ", filename);
@@ -73,4 +80,12 @@ void texturePositionsData(char *rad, struct up_generic_list *texturePosList)
 
     sscanf(rad,"%f %f",&texturePositions.x,&texturePositions.y);
     fprintf(stderr, "Texture. X of obj: %f, Y of obj: %f\n",texturePositions.x,texturePositions.y);
+}
+
+void normalPositionsData(char *rad, struct up_generic_list *normalPosList)
+{
+    struct up_vec3 normalPositions;
+
+    sscanf(rad,"%f %f %f",&normalPositions.x,&normalPositions.y, &normalPositions.z);
+    fprintf(stderr, "normal. X of obj: %f, Y of obj: %f, Z of obj: %f\n",normalPositions.x,normalPositions.y,normalPositions.z);
 }
