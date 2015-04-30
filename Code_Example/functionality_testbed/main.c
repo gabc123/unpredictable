2d4d67c4fcd057d202123dd51bd2b953ecb6093b
o_0 <somtingsomting@gmail.com>
2015-04-30 13:35:48 +0200

    Cleaned up main, added comments, and general house keeping

--- Code_Example/functionality_testbed/main.c
+++ Code_Example/functionality_testbed/main.c
@@ -29,140 +29,102 @@ int main(int argc, char const *argv[])
 	UP_openGLwindowSetup(screen_width,screen_hight,"Det fungerar !!!");
 	printf("opengl window setup done\n");
 
-
-	up_mesh_start_setup(40);
-    up_texture_start_setup();
-
-    //struct up_mesh *mesh = meshTriangleShip();
-    //struct up_mesh *mesh = meshPyramidShip();
-
-    struct up_objModel *testObj = up_loadObjModel("space_mats2.obj");
-    //struct up_objModel *testObj = up_loadObjModel("fighter.obj");
-
-    //struct up_mesh *mesh = UP_mesh_new(testObj->vertex, testObj->vertex_length, testObj->indexArray, testObj->index_length);
-
-    up_objModelFree(testObj);
-
-
-    //struct up_texture_data *texture = up_load_texture("lala.png");
-
-    //struct up_texture_data *texture = up_load_texture("fighter.png");
-
-
-    struct up_modelRepresentation model;
-
     up_network_start_setup();
     
+    int mesh_capacity = 40;
+	up_mesh_start_setup(mesh_capacity);    // opengl setup, and allocate memory for mesh_capacity number of models
+    up_texture_start_setup();               // opengl texture setup
     
-    
+    //this will load all the assets (modouls,texturs) specifyed in objIndex
+    //be aware that index 0 is always a placeholder for modouls not found and so on
     struct up_assets *assets = up_assets_start_setup();
-    struct up_mesh *mesh = &assets->meshArray[0];
-    struct up_texture_data *texture = &assets->textureArray[0];
+   
+    struct up_mesh *mesh = &assets->meshArray[1];
+    struct up_texture_data *texture = &assets->textureArray[1];
 
+    
+    struct up_modelRepresentation model = {0};
 
-    model.pos.x=0;
-    model.pos.y=0;
-    model.pos.z=1;
+    model.scale=assets->scaleArray[1];
 
-    model.rot.x=0;
-    model.rot.y=0;
-    model.rot.z=0;
-
-    model.scale=assets->scaleArray[0];
-
-    /*
-     void up_matrixModel(up_matrix4_t *modelMatrix, struct up_vec3 *pos,struct up_vec3 *rotation,struct up_vec3 *scale);
-     void up_matrixView(up_matrix4_t *matrixView, struct up_vec3 *eye, struct up_vec3 *center,struct up_vec3 *UP);
-
-     void up_matrixPerspective(up_matrix4_t *perspective, GLdouble fov,GLdouble aspectRatio,GLdouble zNear,GLdouble zFar);
-
-
-     void up_getModelViewPerspective(up_matrix4_t *mvp,
-     up_matrix4_t *modelMatrix,
-     up_matrix4_t *matrixView,
-     up_matrix4_t *perspective);
-     */
-
+    // all the 4by4 matrix that are needed to place the model at the right location and with the right perspective
     up_matrix4_t transform ;//= up_matrixModel(&model.pos, &model.rot, &model.scale);
     up_matrix4_t modelMatrix;
     up_matrix4_t viewMatrix;
     up_matrix4_t perspectiveMatrix;
 
-
+    // sets the initial location of the camera,
+    // first 3 values are the camera position,
+    // the next 3 value is where it looking at
+    // the last 3 values is what is up and what is down
     struct up_camera cam = {{0,0,-3},{0,0,1},{0,1,0}};
 
+    // generate the view matrix
     up_matrixView(&viewMatrix, &cam.eye, &cam.center, &cam.up);
 
+    // this is the perspectiv configurations,
+    // field of view(fov),aspect ratio,how close object can be(can not be 0!!!),and how far we can see
     struct up_perspective pers = {70.0f,screen_width/screen_hight,0.01f,1000.0f};
-
-
+    
+    // generate the oersoective matrix
     up_matrixPerspective(&perspectiveMatrix, pers.fov, pers.aspectRatio, pers.zNear, pers.zFar);
+    
     dispMat(&perspectiveMatrix);
-    struct up_ship ship;
-    ship.pos.x = 0;
-    ship.pos.y = 0;
+    
+    // this is the start ship, initilazing the startin positions
+    struct up_ship ship = {0};
     ship.pos.z = 10;
 
-    ship.dir.x = 0;
-    ship.dir.y = 0;
-    ship.dir.z = 0;
-
-    ship.speed = 0;
-
+    // the ship will stand stilll at the begining
     struct shipMovement movement = {0,0,0,0};
 
-    struct shader_module *shaderprog;
-    shaderprog = UP_Shader_new("shadertest",0);
-    printf("Shader finnished\n");
-    //up_matrix4_t sunModelMatrix;
-
-
+    
     //up_matrix4_t identity = up_matrix4identity();
-    //up_network_start_setup();
-
-    // Load a shader just for the menu system
+    
+    // Load a shader just for the menu system (location 0)
     struct shader_module *shader_menu;
-    shader_menu = UP_Shader_new("shader_menu",1);
+    shader_menu = UP_Shader_new("shader_menu",0);
     printf("Shader menu finnished\n");
 
+    // start the menu, and display it
     status=up_menu(shader_menu);
 
+    // loads the shaders for the rendering loop (location 1)
+    struct shader_module *shaderprog;
+    shaderprog = UP_Shader_new("shadertest",1);
+    printf("Shader finnished\n");
 
 
-
+    // starts the main game loop
 	while(status)
 	{
         up_updateFrameTickRate();
         status = UP_eventHandler(&ship,&movement);
 
 		UP_renderBackground();                      //Clears the buffer and results an empty window.
-		UP_shader_bind(shaderprog);                 //
+		UP_shader_bind(shaderprog);                 // tells the gpu what shader program to use
         up_texture_bind(texture, 0);
 
-
-        up_newtwork_getNewMovement(&ship);
+        up_newtwork_getNewMovement(&ship);          // retrive any updates from the network
         
         up_updateShipMovment(&ship);
-        up_updatShipMatrixModel(&modelMatrix,&model,&ship);
+        
+        up_updatShipMatrixModel(&modelMatrix,&model,&ship); // creates the modelMatrix for the ship
 
+        up_matrixView(&viewMatrix, &cam.eye, &cam.center, &cam.up); // creates the view matrix, from the camera
 
-        up_matrixView(&viewMatrix, &cam.eye, &cam.center, &cam.up);
-
-
+        // combinds the 3 matrix modelMatrix, viewMatrix, perspectiveMatrix, into a transformMatrix,
+        // this is needed for the gpu to place the model at the right location on screen and get the right perspective
         up_getModelViewPerspective(&transform, &modelMatrix, &viewMatrix, &perspectiveMatrix);
-        //up_getModelViewPerspective(&transform, &modelMatrix, &viewMatrix, &identity);
-        //dispMat(&transform);
-        UP_shader_update(shaderprog,&transform);
 
+        UP_shader_update(shaderprog,&transform);    // this uploads the transform to the gpu, and will now be applayed to up_draw_mesh
 
-        //up_matrixView(&sunViewMatrix, &cam.center, &cam.eye, &cam.up);
-        //up_getModelViewPerspective(&sunMatrix, &identity, &identity, &identity);
-        up_shader_update_sunligth(shaderprog, &modelMatrix);
-        up_draw_mesh(mesh);
-		UP_openGLupdate();
+        up_shader_update_sunligth(shaderprog, &modelMatrix);    // this uploads the sun angle to the gpu
+        
+        up_draw_mesh(mesh); // this draws the model onto the screen , at the location transform, and with the sunlight modelMatrix
+		UP_openGLupdate();  // this swaps the render and window buffer , displaying it on screen
 
-        //transform = up_matrixModel(&model.pos, &model.rot, &model.scale);
-	}
+    }
 	printf("Ended main loop\n");
 
 	//cleanup and release all data (notice the reverse order of setup)
