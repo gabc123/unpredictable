9a19dbc38e6bb0d2cb34ff8c21c7cdd222a39ae6
tobde <tobde@users.noreply.github.com>
2015-05-05 10:45:32 +0200

    up_actionState added to up_ship.h

--- Code_Example/functionality_testbed/up_ship.h
+++ Code_Example/functionality_testbed/up_ship.h
@@ -3,6 +3,36 @@
 #include "up_matrixTransforms.h"
 #include "up_modelRepresentation.h"
 
+enum shootingStates
+ {
+     none,
+     fireMissile,
+     fireBullet,
+     fireLaser
+ };
+
+ enum movement
+ {
+     none,
+     forwrd,
+     backwrd
+ };
+
+ enum turning
+ {
+     none,
+     left,
+     right
+ };
+
+struct up_actionState
+{
+    enum shootingStates fireWeapon;
+    enum movement engineState;
+    enum turning maneuver;
+    int objectID;
+};
+
 void up_updatShipMatrixModel(up_matrix4_t *matrixModel,struct up_modelRepresentation *model,struct up_objectInfo *ship);
 void up_updateShipMovment(struct up_objectInfo *ship);
 void up_updateFrameTickRate();
