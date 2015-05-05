30f32b0d8dac26b70f55cfbd6d52e743045922a9
tobde <tobde@users.noreply.github.com>
2015-05-05 15:53:32 +0200

    Sunny editing in works

--- Code_Example/functionality_testbed/up_star_system.c
+++ Code_Example/functionality_testbed/up_star_system.c
@@ -17,10 +17,10 @@
 
 void up_generate_map()
 {
-    
-    
-    
-    
+
+
+
+
 }
 
 void up_generate_sun()
@@ -29,9 +29,9 @@ void up_generate_sun()
     sun.scale.x = 1;
     sun.scale.y = 1;
     sun.scale.z = 1;
-    
-    sun.modelId = 0;
-    
+
+    sun.modelId = 3;
+
     if(up_unit_add(sun)==0){
         UP_ERROR_MSG("could not add sun");
     }
@@ -40,7 +40,7 @@ void up_generate_sun()
 
 void up_generate_asteroidBelt(int density,float maxAngle,float minAngle,float outerEdge,float innerEdge,float maxHeight,float minHeight)
 {
-    
+
     srand((unsigned)time(NULL));
     struct up_objectInfo asteroid;
     float angle=0;
@@ -50,32 +50,32 @@ void up_generate_asteroidBelt(int density,float maxAngle,float minAngle,float ou
     float dy=0;
 
     int i=0;
-    
+
     asteroid.modelId=2;
-    
+
     for(i=0; i<density; i++){
-        
+
         angle = minAngle + ((float)(rand()%UP_RAND_FLOAT_PRECISION) / UP_RAND_FLOAT_PRECISION) * (maxAngle - minAngle);
         height = minHeight + ((float)(rand()%UP_RAND_FLOAT_PRECISION) / UP_RAND_FLOAT_PRECISION) * (maxHeight - minHeight);
         radius = innerEdge + ((float)(rand()%UP_RAND_FLOAT_PRECISION) / UP_RAND_FLOAT_PRECISION) * (outerEdge - innerEdge);
-        
+
         dx = cosf(angle);
         dy = sinf(angle);
-        
+
         asteroid.pos.x = radius * dx;
         asteroid.pos.y = radius * dy;
         asteroid.pos.z = height;
-        
+
         asteroid.scale.x = 1.0 + ((float)(rand()%UP_RAND_FLOAT_PRECISION) / UP_RAND_FLOAT_PRECISION) * 0.5f ;
         asteroid.scale.y = 1.0 + ((float)(rand()%UP_RAND_FLOAT_PRECISION) / UP_RAND_FLOAT_PRECISION) * 0.5f ;
         asteroid.scale.z = 1.0 + ((float)(rand()%UP_RAND_FLOAT_PRECISION) / UP_RAND_FLOAT_PRECISION) * 0.5f ;
-        
+
         if(up_unit_add(asteroid)==0){
             UP_ERROR_MSG("could not add asteroid");
             break;
         }
-        
+
     }
-    
-    
+
+
 }
