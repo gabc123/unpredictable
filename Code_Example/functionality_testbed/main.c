9c3eb42d23758be0c6b230b549814df15aa4699a
o_0 <somtingsomting@gmail.com>
2015-05-06 17:15:32 +0200

    fixed crash

--- Code_Example/functionality_testbed/main.c
+++ Code_Example/functionality_testbed/main.c
@@ -138,6 +138,7 @@ int main(int argc, char const *argv[])
     up_weaponCoolDown_start_setup(&currentEvent);
     printf("out of weapon\n");
 
+    struct up_eventState funkarEj = {0};
     // starts the main game loop
     while(status)
     {
@@ -151,7 +152,7 @@ int main(int argc, char const *argv[])
 
         //up_updatShipMatrixModel(&modelMatrix,&model,ship); // creates the modelMatrix for the ship
         //up_updateShipMovment(ship);
-        up_update_actions(&shipAction, NULL, 0);
+        up_update_actions(&shipAction, NULL, 0,&funkarEj);
         up_updateMovements();
 
         up_update_camera(&cam, ship);
