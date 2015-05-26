e2c60d43c0421b730153573fb22f06bb9f5b97d1
gabc123 <somtingsomting@gmail.com>
2015-05-26 22:08:22 +0200

    same

--- server_test_udp/up_network_packet_utilities.h
+++ server_test_udp/up_network_packet_utilities.h
@@ -17,12 +17,9 @@
 #define UP_REGISTRATE_FLAG (unsigned char)1
 #define UP_LOGIN_FLAG (unsigned char)2
 #define UP_USER_PASS_FLAG (unsigned char)4
-#define UP_USERPASS 21
-#define UP_USER_NAME_PASS_MAX 255
-
-#define REGSUCCESSS (unsigned char) 5
+#define REGSUCESSS (unsigned char) 5
 #define REGFAILED (unsigned char) 6
-#define LOGINSUCCESS (unsigned char) 7
+#define LOGINSUCESS (unsigned char) 7
 #define LOGINFAILED (unsigned char) 8
 
 
@@ -33,17 +30,9 @@
 
 // maintenance flags
 #define UP_PACKET_HEARTBEAT_FLAG (unsigned char)40
+#define UP_MAP_DATA_FLAG (unsigned char)50
 
-// warning:
-// this function copy the data from source into destination,
-// this function is pure evil and copy raw byte data of element_size,
-// thid function do not check if destination and source is pointing to valid data
-// and it do not do any bound checking
-void generic_copyElement(unsigned int element_size,unsigned char *destination,unsigned char *source);
-
-unsigned int up_copyObjectIntoBuffer(struct objUpdateInformation *object,unsigned char *buffer);
-unsigned int  up_copyBufferIntoObject(unsigned char *buffer,struct objUpdateInformation *object);
-
+#define UP_MAP_DATA_FLAG (unsigned char)50
 
 struct up_packet_movement
 {
@@ -56,13 +45,10 @@ struct up_packet_movement
 };
 
 
-int up_network_heartbeat_packetEncode(unsigned char *data, int timestamp);
-int up_network_heartbeat_packetDecode(unsigned char *data, int *timestamp);
-
-int up_network_logInRegistrate_packetEncode(unsigned char *data, int clientId, unsigned char regLogFlag);
-int up_network_logInRegistrate_packetDecode(unsigned char *data, int *clientId, unsigned char *regLogFlag);
 
 
+int up_network_heartbeat_packetEncode(unsigned char *data,int timestamp);
+int up_network_heartbeat_packetDecode(unsigned char *data,int *timestamp);
 
 
 int up_network_objectmove_packetEncode(struct objUpdateInformation *object,
